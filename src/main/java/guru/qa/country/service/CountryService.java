436710da055525dddaf8dfd1e5bead7267b9be0c
a.zavrichko <andrey@zavrichko.dev>
2024-11-28 17:05:58 +0200

    ДЗ-21

--- src/main/java/guru/qa/country/service/CountryService.java
+++ src/main/java/guru/qa/country/service/CountryService.java
@@ -1,11 +1,11 @@
 package guru.qa.country.service;
 
+import guru.qa.country.data.CountryEntity;
 import guru.qa.country.data.CountryRepository;
 import guru.qa.country.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -20,8 +20,26 @@ public class CountryService {
 
   public List<Country> getAllCountries() {
     return countryRepository.findAll()
-        .stream()
-        .map(Country::fromEntity)
-        .toList();
+            .stream()
+            .map(Country::fromEntity)
+            .toList();
+  }
+
+  public Country addCountry(Country country) {
+    CountryEntity entity = new CountryEntity();
+    entity.setCountryName(country.countryName());
+    entity.setCountryCode(country.countryCode());
+    entity = countryRepository.save(entity);
+    return Country.fromEntity(entity);
+  }
+
+  public Country updateCountryName(String countryCode, String newCountryName) {
+    CountryEntity entity = countryRepository.findByCountryCode(countryCode);
+    if (entity == null) {
+      throw new IllegalArgumentException("Country with code " + countryCode + " not found.");
+    }
+    entity.setCountryName(newCountryName);
+    entity = countryRepository.save(entity);
+    return Country.fromEntity(entity);
   }
 }
